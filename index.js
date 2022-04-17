1db0f6ae16bf5d8e23891a2568ee00e17ed57037
Njoki <kinuthiash@gmail.com>
2022-04-17 23:19:08 +0300

    commit tree 4abcfbc4b31f18e2e39b941b683ef1f6ae3a3939

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const input = document.querySelector("#input")
 function addingEventListener() {
-  input.addEventListener('click', function(){
-    alert('I was clicked')
+  input.addEventListener('click', function(e){
+    return alert('I was clicked')
   } )
 }
